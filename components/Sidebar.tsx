3b179c0bf6163853a975ec122e493681b98e38f9
ANU KUMAR SAH <100104302+Anu374@users.noreply.github.com>
2022-05-17 12:01:17 +0530

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -80,6 +80,13 @@ const Sidebar = () => {
         {/* //TODO remove bg black */}
         Night Mode
       </button>
+      
+      <button
+        className="w-8/12 px-5 py-2 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 hover:scale-105 focus:outline-none"
+        onClick={() =>"https://blog-site-ptnyujz2s-anu374.vercel.app/"
+      >
+        Blog
+      </button>
     </>
   );
 };

d6a645814ab259ed24401f01799d8c07289cbeac
ANU KUMAR SAH <100104302+Anu374@users.noreply.github.com>
2022-05-17 12:04:20 +0530

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -81,12 +81,14 @@ const Sidebar = () => {
         Night Mode
       </button>
       
-      <button
-        className="w-8/12 px-5 py-2 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 hover:scale-105 focus:outline-none"
-        onClick={() =>"https://blog-site-ptnyujz2s-anu374.vercel.app/"
+       <a
+        href="https://blog-site-ptnyujz2s-anu374.vercel.app/"
+        download="Anu-resume.pdf"
+        className="flex items-center justify-center px-2 py-1 my-2 bg-gray-200 rounded-full cursor-pointer dark:bg-dark-200 dark:bg-black-500"
       >
-        Blog
-      </button>
+        <GiTie className="w-6 h-6" />
+        <span>Blog</span>
+      </a>
     </>
   );
 };

498a40e8badac877e32a0bae9c4cdaa77719bc8d
ANU KUMAR SAH <100104302+Anu374@users.noreply.github.com>
2022-05-17 13:58:32 +0530

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -75,14 +75,14 @@ const Sidebar = () => {
         Email me
       </button>
       
-/*<div>
+      
       <button
-        className="w-8/12 px-5 py-2 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 hover:scale-105 focus:outline-none"
+        className="my-8px w-8/12 px-5 py-2 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 hover:scale-105 focus:outline-none"
         onClick={() => window.open("https://blog-site-mu.vercel.app/")}
       >
         Blog
       </button>
-      </div>*/
+      
       <button
         onClick={changeTheme}
         className="w-8/12 px-5 py-2 my-4 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 focus:outline-none hover:scale-105 "

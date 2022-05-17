c5ba1319886e95d9016d1c862e4231401b56072f
ANU KUMAR SAH <100104302+Anu374@users.noreply.github.com>
2022-05-17 12:22:12 +0530

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,6 +3,7 @@ import { GiTie } from "react-icons/gi";
 import { GoLocation } from "react-icons/go";
 import { useTheme } from "next-themes";
 import Image from "next/image";
+import { FaBlog } from 'react-icons/fa';
 
 
 
@@ -73,12 +74,9 @@ const Sidebar = () => {
       >
         Email me
       </button>
-      <button
-        className="w-8/12 px-5 py-2 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 hover:scale-105 focus:outline-none"
-        onClick={() => window.open("https://blog-site-ptnyujz2s-anu374.vercel.app/")}
-      >
-        Blog
-      </button>
+      <a href="https://blog-site-ptnyujz2s-anu374.vercel.app/" >
+          <FaBlog className="w-8 h-8 cursor-pointer" />
+        </a>
       
       <button
         onClick={changeTheme}
